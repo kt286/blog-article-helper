2be7933615129684d328635f5567acf5f29ccce8
xu yanyun <xuyanyun@trans-cosmos.com.cn>
2018-01-12 11:35:15 +0800

    添加CSDN博客自动查看全文

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# juejinHelper
-掘金文章直接跳转到原文，免得又是少图片 又是乱格式
+# blog-article-helper
+博客文章直接跳转到来源地址、自动点击查看全文
+<br><br>
+
+### 使用方法 ###
+
+1. Chrome浏览器（安装下面其中一个插件）：
+- Violentmonkey（暴力猴）<sup>推荐</sup>  [点我下载](https://chrome.google.com/webstore/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag)
+- Tampermonkey（油猴） [点我下载](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+2. 安装脚本 [点我安装](https://raw.githubusercontent.com/kt286/blog-article-helper/master/blog-article-helper.user.js)
+
+### 用户协议
+MIT License
---
+++ blog-article-helper.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         blog-article-helper
+// @description  博客文章直接跳转到来源地址、自动点击查看全文
+// @author       Cloud
+// @version      0.2
+// @namespace    https://github.com/kt286/blog-article-helper
+// @homepageURL  https://github.com/kt286/blog-article-helper
+// @supportURL   https://github.com/kt286/blog-article-helper/issues
+// @include      *://juejin.im/entry/*
+// @include      *://blog.csdn.net/*/article/details/*
+// @updateURL    https://raw.githubusercontent.com/kt286/blog-article-helper/master/blog-article-helper.user.js
+// @grant none
+// ==/UserScript==
+const host = location.hostname;
+
+if (host.indexOf("juejin.im") != -1) {
+    //掘金跳转到来源地址，免得又是少图片 又是乱格式
+    window.location.href = decodeURIComponent(document.querySelector(".originalUrl a").href.replace("https://link.juejin.im/?target=", ""));
+} else if (host.indexOf("blog.csdn.net") != -1) {
+    window.onload = () => {
+        document.querySelector(".read_more_btn").click(); //查看全文
+    }
+}
